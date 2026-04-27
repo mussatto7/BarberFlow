e91041cd55a70dd315deab87589d2e69953109d2
Lucas Mussatto Rodrigues <lucasmussatto71@gmail.com>
2026-04-27 20:26:24 -0300

    Arquivos iniciais

---
+++ src/main/java/com/devLucas/BarberFlow/BarberFlowApplication.java
@@ -0,0 +1,13 @@
+package com.devLucas.BarberFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devLucas/BarberFlow/BarberFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devLucas.BarberFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
